5ed4f29dc7a7d883915949b953247280e25e8e65
Leudi Rosario <leudi_rosario@hotmail.com>
2019-11-20 17:19:33 -0400

    changing formproduct

--- src/components/ProductForm.js
+++ src/components/ProductForm.js
@@ -1,9 +1,75 @@
-import React, { Component } from "react";
-
+import React, {
+  Component
+} from "react";
+//   importaciones para trabajar con los formularios son 
+import {
+  Button,
+  Form,
+  FormGroup,
+  Input,
+  Label
+} from 'reactstrap'
 class ProductForm extends Component {
-  render() {
-    return <h1>Hola Mundo</h1>;
+  state = {
+    id: 0,
+    name: '',
+    unitPrice: 0,
+    unitsInStock: 0
   }
-}
+  componentDidMount() {
+    if (this.props.product) {
+      this.setState({
+        id: this.props.product.id,
+        name: this.props.product.name,
+        unitPrice: this.props.product.unitPrice,
+        unitsInStock: this.props.product.unitsInStock
+      });
+    }
 
-export default ProductForm;
+    onchange = e => {
+      this.setState({
+        [e.target.name]: e.target.value
+      });
+    };
+
+    OnSubmitNew = e => {
+      e.preventDefault();
+      this.props.addProduct(this.state);
+    };
+
+
+    onSubmitUpdate = e => {
+      e.preventDefault();
+      this.props.updateProduct(this.state);
+    };
+
+
+
+
+  }
+
+
+
+
+
+  render() {
+    return ( < Form >
+      <
+      FormGroup >
+      <
+      label
+      for = "id" > ID < /label> <
+      input type = "text"
+      name = "id"
+      onChange = {
+        this.OnChange
+      }
+      value = {
+        this.state.id
+      } > < /input> <
+      /FormGroup> <
+      /Form>)
+    }
+  }
+
+  export default ProductForm;
