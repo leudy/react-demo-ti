fb9af30ae75925be12aaf5738a531a5413755de8
Leudi Rosario <leudi_rosario@hotmail.com>
2019-11-21 17:33:18 -0400

    mf-form

--- src/components/ProductForm.js
+++ src/components/ProductForm.js
@@ -1,21 +1,13 @@
-import React, {
-  Component
-} from "react";
-//   importaciones para trabajar con los formularios son 
-import {
-  Button,
-  Form,
-  FormGroup,
-  Input,
-  Label
-} from 'reactstrap'
+import React, { Component } from 'react';
+//   importaciones para trabajar con los formularios son
+import { Button, Form, FormGroup, Input, Label } from 'reactstrap';
 class ProductForm extends Component {
   state = {
     id: 0,
     name: '',
     unitPrice: 0,
     unitsInStock: 0
-  }
+  };
   componentDidMount() {
     if (this.props.product) {
       this.setState({
@@ -25,51 +17,48 @@ class ProductForm extends Component {
         unitsInStock: this.props.product.unitsInStock
       });
     }
-
-    onchange = e => {
-      this.setState({
-        [e.target.name]: e.target.value
-      });
-    };
-
-    OnSubmitNew = e => {
-      e.preventDefault();
-      this.props.addProduct(this.state);
-    };
-
-
-    onSubmitUpdate = e => {
-      e.preventDefault();
-      this.props.updateProduct(this.state);
-    };
-
-
-
-
   }
 
+  onchange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
 
+  OnSubmitNew = e => {
+    e.preventDefault();
+    this.props.addProduct(this.state);
+  };
 
-
+  onSubmitUpdate = e => {
+    e.preventDefault();
+    this.props.updateProduct(this.state);
+  };
 
   render() {
-    return ( < Form >
-      <
-      FormGroup >
-      <
-      label
-      for = "id" > ID < /label> <
-      input type = "text"
-      name = "id"
-      onChange = {
-        this.OnChange
-      }
-      value = {
-        this.state.id
-      } > < /input> <
-      /FormGroup> <
-      /Form>)
-    }
+    return (
+      <Form>
+        <FormGroup>
+          <label for="id">ID</label>
+          <input
+            type="number"
+            name="id"
+            onChange={this.OnChange}
+            value={this.state.id}
+          ></input>
+        </FormGroup>
+        <FormGroup>
+          <label for="name">Nombre</label>
+          <input
+            type="text"
+            name="name"
+            onChange={this.OnChange}
+            value={this.state.name}
+          ></input>
+        </FormGroup>
+      </Form>
+    );
   }
+}
 
-  export default ProductForm;
+export default ProductForm;
